9c9bbe17c424ec2db39891f8debca0fbda222bee
Renan3223 <82404084+Renan3223@users.noreply.github.com>
2021-07-13 21:41:11 -0300

    sobre usuario renan

--- SOS_Estagios/src/app/app-routing.module.ts
+++ SOS_Estagios/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'who',
+    loadChildren: () => import('./pages/who/who.module').then( m => m.WhoPageModule)
   }
+
 ];
 @NgModule({
   imports: [
